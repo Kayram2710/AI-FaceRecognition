9199b50e1fd5ca8ab42114bb7424cfa7a4410b01
Karim <kimomomo@hotmail.com>
2024-05-27 17:10:00 -0400

    Ran the script to obtain a cleaned data set for happy, neutral, and angry
    Cleaned refers to a standardized data set, the raw data was installed locally and obtained from: https://www.kaggle.com/datasets/jonathanoheix/face-expression-recognition-dataset?resource=download

--- DataCleaner.py
+++ DataCleaner.py
@@ -1,17 +1,25 @@
 import os
 from PIL import Image
 
-rawPath = "Raw Dataset"
-destinationPath = "Cleaned Dataset"
-size=(256, 256)
+#Defined paths
+rawPath = "Raw Dataset" #Source for Raw Data, creating String for path
+destinationPath = "Cleaned Dataset" #Destination for cleaned Data, creating String for path
+size=(256, 256) #Chosen size that pictures will be formatted too
 
-#Iterate over files in directory
+#creating a value for total amount of pictures desired to be taken from each folder
+total = 500
+
+#Iterating over all sub directories in main directory (Raw Data Folder)
 for category in os.listdir(rawPath):
-    source = os.path.join(rawPath, category)
-    destination = os.path.join(destinationPath, category)
+    source = os.path.join(rawPath, category) #Creating String source as path for current sub directory
+    destination = os.path.join(destinationPath, category) #Creating String destination as relative destination path for current sub directory
 
+    #creating an iterator count
+    count = 0
+
+    #Looping through every image in current sub category
     for imageFile in os.listdir(source):
-        #Open the image to edit
+        #Open the image to edit (Using PIL)
         image = Image.open(os.path.join(source, imageFile))
 
         #Edit the image
@@ -20,4 +28,11 @@ for category in os.listdir(rawPath):
 
         #Save the processed image
         image.save(os.path.join(destination, imageFile))
+        count = count + 1
 
+        #Status report of progress
+        print(f"Progress for {category}: {((count/total)*100):.2f}% transferred")
+
+        #Break when total is reached
+        if(count == total):
+            break
